5e53a25bb1a6979136b3c4f74d8f1eec8932307b
user <user@DESKTOP-GI6P2JA>
2024-01-01 22:24:34 +0300

    Add project files.

---
+++ Odev/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Sayı Bilme Oyununa Hoş Geldiniz!");
+
+        Console.WriteLine("Zorluk seviyesini seçin: 1-Kolay 2-Orta 3-Zor");
+        int zorlukSeviyesi = Convert.ToInt32(Console.ReadLine());
+
+        int min, max;
+
+        switch (zorlukSeviyesi)
+        {
+            case 1:
+                min = 1;
+                max = 10;
+                break;
+            case 2:
+                min = 1;
+                max = 50;
+                break;
+            case 3:
+                min = 1;
+                max = 100;
+                break;
+            default:
+                Console.WriteLine("Geçersiz zorluk seviyesi. Varsayılan olarak orta seviye seçili.");
+                min = 1;
+                max = 50;
+                break;
+        }
+
+        int dogruCevap = new Random().Next(min, max + 1);
+        int puan = 0;
+
+        while (true)
+        {
+            Console.WriteLine($"Tahminin ({min}-{max}): ");
+            int tahmin;
+
+            while (!int.TryParse(Console.ReadLine(), out tahmin) || tahmin < min || tahmin > max)
+            {
+                Console.WriteLine($"Kör müsün? Lütfen {min}-{max} arasında bir sayı gir.");
+            }
+
+            if (tahmin == dogruCevap)
+            {
+                puan += 100;
+                Console.WriteLine($"Tebrikler! Doğru tahmin. Puanınız: {puan}");
+                break;
+            }
+            else
+            {
+                puan -= 3;
+                Console.WriteLine($"Yanlış tahmin. Puanınız: {puan}");
+            }
+        }
+    }
+}
